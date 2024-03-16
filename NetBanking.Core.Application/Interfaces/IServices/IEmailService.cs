804d34a6fba2fe4beeced43f418cb6a897583038
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2024-03-15 23:21:39 -0400

    Add project files.

---
+++ NetBanking.Core.Application/Interfaces/IServices/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using NetBanking.Core.Application.Dtos.Email;
+
+namespace NetBanking.Core.Application.Interfaces.IServices
+{
+    public interface IEmailService
+    {
+        Task SendAsync(EmailRequest request);
+    }
+}
---
+++ NetBanking.Infrastructure.Identity/ServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBanking.Infrastructure.Identity
+{
+    public class ServiceRegistration
+    {
+
+    }
+}
