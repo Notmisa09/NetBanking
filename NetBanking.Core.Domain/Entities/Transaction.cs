ec735d23f364da7257fa49be5a5b110fb161b728
G3remy <20222117@itla.edu.do>
2024-03-17 14:25:06 -0400

    Domain classes completed.

---
+++ NetBanking.Core.Domain/Entities/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using NetBanking.Core.Domain.Common;
+using NetBanking.Core.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBanking.Core.Domain.Entities
+{
+    public class Transaction : AuditableEntity
+    {
+        public string EmissorProductId { get; set; }
+        public string ReceiverProductId { get; set; }
+        public decimal Cantity { get; set; }
+        public TransactionType Type { get; set; }
+
+    }
+}
---
+++ NetBanking.Core.Domain/Enums/TransactionType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBanking.Core.Domain.Enums
+{
+    public enum TransactionType
+    {
+        ExpressPay,
+        CreditCardPay,
+        LoanPay,
+        BeneficiaryPay,
+        CashAdvance,
+        SimpleTransaction
+    }
+}
