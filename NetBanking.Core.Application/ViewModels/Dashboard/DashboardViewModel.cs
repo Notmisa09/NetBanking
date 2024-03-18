d0dd0788e488599637654a67ee91468f50438651
Yahinniel Alejandro Torres Vásquez <20221996@itla.edu.do>
2024-03-17 22:47:56 -0500

    Agregando los requerimientos del admin.

---
+++ NetBanking.Core.Application/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBanking.Core.Application.ViewModels.Dashboard
+{
+    public class DashboardViewModel
+    {
+        public int AllTransaction { get; set; }
+        public int AllPaymentsNumber { get; set; }
+        public int ActiveClients { get; set; }
+        public int InactiveClients { get; set; }
+        public int AllPayments { get; set; }
+        public int AssignedProduct { get; set; }
+    }
+}
---
+++ NetBanking.Core.Application/ViewModels/Dashboard/ListUserRegisterAndStateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBanking.Core.Application.ViewModels.Dashboard
+{
+    public class ListUserRegisterAndStateViewModel
+    {
+        List<UserViewModel> userRegister {  get; set; }
+    }
+}
