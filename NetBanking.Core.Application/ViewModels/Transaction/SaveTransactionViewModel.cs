84db2a74dce4eee9a9fff555bf2236e6eefaaedb
Yahinniel Alejandro Torres Vásquez <20221996@itla.edu.do>
2024-03-17 19:01:15 -0500

    Agregamos los ViewModels necesarios.

--- NetBanking.Core.Application/ViewModels/Transaction/SaveTransactionViewModel.cs
+++ NetBanking.Core.Application/ViewModels/Transaction/SaveTransactionViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace NetBanking.Core.Application.ViewModels.Transaction
+﻿using NetBanking.Core.Domain.Enums;
+
+namespace NetBanking.Core.Application.ViewModels.Transaction
 {
     public class SaveTransactionViewModel
     {
+        public string EmissorProductId { get; set; }
+        public string ReceiverProductId { get; set; }
+        public decimal Cantity { get; set; }
+        public TransactionType Type { get; set; }
     }
 }
--- NetBanking.Core.Domain/Entities/Beneficiary.cs
+++ NetBanking.Core.Domain/Entities/Beneficiary.cs
@@ -1,15 +1,10 @@
 ﻿using NetBanking.Core.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetBanking.Core.Domain.Entities
 {
     public class Beneficiary : AuditableEntity
     {
         public string AccountId { get; set; }
-        public string BeneficiaryId { get; set; }
+        public string BeneficiaryId { get; set; } 
     }
 }
