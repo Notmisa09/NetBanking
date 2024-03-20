4741ae9b58a8cdc692756d902b124a6337cf343c
Yahinniel Alejandro Torres Vásquez <20221996@itla.edu.do>
2024-03-20 12:49:26 -0500

    Agregando el GetById

--- NetBanking.Core.Application/Services/ClientServices.cs
+++ NetBanking.Core.Application/Services/ClientServices.cs
@@ -22,11 +22,38 @@ namespace NetBanking.Core.Application.Services
         {
             _accountService = accountService;
             _mapper = mapper;
-            _trasactionRepository = trasactionRepository;
-            _creditCardRepository = creditCardRepository;
-            _loanRepository = loanRepository;
-            _savingsAccountRepository = savingsAccountRepository;
+            _httpContextAccessor = httpContextAccessor;
+            _loanService = loanService;
+            _savingsAccountService = savingsAccountService;
         }
+        
+        //public async Task<GetAllProductsByClientViewModel> GetAllProductsByClientAsync()
+        //{
+        //    var userId = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user").Id;
+        //    GetAllProductsByClientViewModel vm = new()
+        //    {
+        //        SavingsAccounts = await _savingsAccountService.GetByOwnerIdAsync(userId),
+        //        CreditCards = await _creditCardService.GetByOwnerIdAsync(userId),
+        //        Loans = await _loanService.GetByOwnerIdAsync(userId)
+        //    };
+            
+        //    return vm;
+        //}
+
+        //public async Task<List<BeneficiaryViewModel>> GetAllBeneficiariesByClientAsync()
+        //{
+        //    var userId = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user").Id;
+        //    return await _beneficiaryService.GetByOwnerIdAsync(userId);
+        //}
+
+        //public async Task RealizeTransaction(SaveTransactionViewModel svm)
+        //{
+        //    if(svm.Type == Domain.Enums.TransactionType.ExpressPay)
+        //    {
+        //        svm.EmissorProductId = "1";
+        //    }
+        //    await _transactionService.AddAsync(svm);
+        //}
 
         public async Task<List<UserViewModel>> GetAllClientProducts()
         {
