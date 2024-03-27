7610836d1988486a36931f4a693e5b1069476469
G3remy <20222117@itla.edu.do>
2024-03-26 22:09:54 -0400

    Pequeños cambios

--- WebApp/Controllers/UserController.cs
+++ WebApp/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using NetBanking.Core.Application.Helpers;
 using NetBanking.Core.Application.Interfaces.Services;
 using NetBanking.Core.Application.Singelton;
 using NetBanking.Core.Application.ViewModels.Users;
-
+using WebApp.Middlewares;
 
 namespace WebApp.Controllers
 {
@@ -25,6 +25,7 @@ namespace WebApp.Controllers
         }
 
         //INDEX
+        [ServiceFilter(typeof(LoginAuthorize))]
         public IActionResult Index()
         {
             if (string.IsNullOrEmpty(TempData["Success"]?.ToString()))
@@ -37,6 +38,7 @@ namespace WebApp.Controllers
             return View(new LoginViewModel());
         }
 
+        [ServiceFilter(typeof(LoginAuthorize))]
         [HttpPost]
         public async Task<IActionResult> Index(LoginViewModel vm)
         {
